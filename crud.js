4af475eeb224a46637fc8b088ce4e475e3f73547
claurojass23 <128934493+claurojass23@users.noreply.github.com>
2023-06-03 16:56:41 -0500

    11commit

--- crud.js
+++ crud.js
@@ -13,51 +13,82 @@ stars.forEach(function(star, index){
 })
 
 
-let personas =[];
-  let miIndex;
+let clients=[];
+  let myIndex;
    function addClient(){
-   
-    let newPersona ={
-      usuario:document.getElementById("client-name").value,
-      correo:document.getElementById("client-email").value
+    
+    let newClient={
+      name:document.getElementById("client-name").value,
+      email:document.getElementById("client-email").value
     }
-    personas.push(newPersona)
+    clients.push(newClient)
     displayClients()
-  }
+}
 
-
-  function displayUsuario(){
+function displayClients(){
     document.getElementById("form-list-client-body").innerHTML=""
-    for (i=0;i<personas.length;i++){
-      let miTr=document.createElement("tr")
-      for(a in personas[i]){
-        let mitd=document.createElement("td")
+    for (i=0;i<clients.length;i++){
+      let myTr=document.createElement("tr")
+      for(a in clients[i]){
+        var mytd=document.createElement("td")
         mytd.innerHTML=clients[i][a]
-        myTr.appendChild(mitd)
+        myTr.appendChild(mytd)
       }
       let actionTd=document.createElement("td")
       let editBtn=document.createElement("button")
-      editBtn.innerHTML="Editar"
+      editBtn.innerHTML="Edit"
       editBtn.setAttribute("class" , "btn btn-sm btn-primary")
-      editBtn.setAttribute("onclick" , "editarClient("+i+")")
-
+      editBtn.setAttribute("onclick" , "editClient("+i+")")
 
       let deletebtn=document.createElement("button")
-      deletebtn.innerHTML="Eliminar"
+      deletebtn.innerHTML="Delete"
       deletebtn.setAttribute("class" , "btn btn-sm btn-danger")
-      deletebtn.setAttribute("onclick" , "eliminarClient("+i+")")
-
+      deletebtn.setAttribute("onclick" , "deleteClient("+i+")")
 
       actionTd.appendChild(editBtn)
       actionTd.appendChild(deletebtn)
       myTr.appendChild(actionTd)
       document.getElementById("form-list-client-body").appendChild(myTr)
 
+    }
+     document.getElementById("client-name").value="";
+     document.getElementById("client-email").value=""
+}
 
-      }
-      document.getElementById("client-name").value="";
-      document.getElementById("client-email").value=""
-  }
+function editClient(i){
+    console.log(clients[i])
+    myIndex=i;
+    let updatebtn=document.createElement("button")
+    updatebtn.innerHTML="Update";
+    updatebtn.setAttribute("class", "btn btn-sm btn-success")
+    updatebtn.setAttribute("onclick","updClient()")
+    document.getElementById("saveupdate").innerHTML=""
+    document.getElementById("saveupdate").appendChild(updatebtn);
+    document.getElementById("client-name").value=clients[i].name
+    document.getElementById("client-email").value=clients[i].email
+}
+
+function updClient(){
+    let updatedClient={
+      name:document.getElementById("client-name").value,
+      email:document.getElementById("client-email").value
+    }
+    clients[myIndex]=updatedClient;
+    let crbtn=document.createElement("button")
+    crbtn.innerHTML="Save";
+    crbtn.setAttribute("onclick","addClient()")
+    crbtn.setAttribute("class","btn btn-sm btn-success")
+    document.getElementById("saveupdate").innerHTML=""
+    
+    document.getElementById("saveupdate").appendChild(crbtn);
+    
+    displayClients()
+}
+
+function deleteClient(i){
+    clients.splice(i,1)
+    displayClients()
+}
 
 
  
