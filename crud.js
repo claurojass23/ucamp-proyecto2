a8721c020b9aabb6d176219088bb642507a18a08
claurojass23 <128934493+claurojass23@users.noreply.github.com>
2023-06-09 22:40:22 -0500

    12commit

--- crud.js
+++ crud.js
@@ -12,70 +12,80 @@ stars.forEach(function(star, index){
     })
 })
 
-
 let clients=[];
   let myIndex;
    function addClient(){
     
-    let newClient={
+    var newClient={
       name:document.getElementById("client-name").value,
       email:document.getElementById("client-email").value
     }
     clients.push(newClient)
     displayClients()
-}
+  }
 
-function displayClients(){
+  function displayClients(){
     document.getElementById("form-list-client-body").innerHTML=""
     for (i=0;i<clients.length;i++){
       let myTr=document.createElement("tr")
       for(a in clients[i]){
-        var mytd=document.createElement("td")
+        let mytd=document.createElement("td")
         mytd.innerHTML=clients[i][a]
         myTr.appendChild(mytd)
       }
+      
       let actionTd=document.createElement("td")
       let editBtn=document.createElement("button")
-      editBtn.innerHTML="Edit"
       editBtn.setAttribute("class" , "btn btn-sm btn-primary")
       editBtn.setAttribute("onclick" , "editClient("+i+")")
 
+      const iconoEdit = document.createElement("i");
+      iconoEdit.classList.add("fa", "fa-edit");
+      editBtn.appendChild(iconoEdit)
+  
       let deletebtn=document.createElement("button")
-      deletebtn.innerHTML="Delete"
       deletebtn.setAttribute("class" , "btn btn-sm btn-danger")
       deletebtn.setAttribute("onclick" , "deleteClient("+i+")")
+      
+
+      const iconoBasura = document.createElement("i");
+      iconoBasura.classList.add("fa", "fa-trash");
+      deletebtn.appendChild(iconoBasura)
+
 
       actionTd.appendChild(editBtn)
       actionTd.appendChild(deletebtn)
       myTr.appendChild(actionTd)
       document.getElementById("form-list-client-body").appendChild(myTr)
 
-    }
-     document.getElementById("client-name").value="";
-     document.getElementById("client-email").value=""
-}
+      }
+      document.getElementById("client-name").value="";
+      document.getElementById("client-email").value=""
+  }
 
-function editClient(i){
+  
+  function editClient(i){
     console.log(clients[i])
     myIndex=i;
     let updatebtn=document.createElement("button")
-    updatebtn.innerHTML="Update";
+    updatebtn.innerHTML="Guardar cambio";
     updatebtn.setAttribute("class", "btn btn-sm btn-success")
     updatebtn.setAttribute("onclick","updClient()")
     document.getElementById("saveupdate").innerHTML=""
     document.getElementById("saveupdate").appendChild(updatebtn);
     document.getElementById("client-name").value=clients[i].name
     document.getElementById("client-email").value=clients[i].email
-}
+  }
 
-function updClient(){
+  
+  function updClient(){
     let updatedClient={
       name:document.getElementById("client-name").value,
       email:document.getElementById("client-email").value
     }
     clients[myIndex]=updatedClient;
     let crbtn=document.createElement("button")
-    crbtn.innerHTML="Save";
+    crbtn.innerHTML="Guardar";
     crbtn.setAttribute("onclick","addClient()")
     crbtn.setAttribute("class","btn btn-sm btn-success")
     document.getElementById("saveupdate").innerHTML=""
@@ -83,12 +93,13 @@ function updClient(){
     document.getElementById("saveupdate").appendChild(crbtn);
     
     displayClients()
-}
+  }
 
-function deleteClient(i){
+  
+  function deleteClient(i){
     clients.splice(i,1)
     displayClients()
-}
+  }
 
 
  
